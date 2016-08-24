309dd24647629b5e91d2820d871d4378827580c2
miaozw <weinkym@qq.com>
2016-08-24 17:49:57 +0800

    增加快速回复的功能，研究同个identifier 不能多次发送，除非调用删除notification

--- ztest/test_QSystemTrayIcon/zdelegate.mm
+++ ztest/test_QSystemTrayIcon/zdelegate.mm
@@ -8,18 +8,22 @@ void testCallBack()
 - (void)userNotificationCenter:(NSUserNotificationCenter *)center didDeliverNotification:(NSUserNotification *)notification
 {
     NSLog(@"通知已经递交！");
+    NSLog(@"identifier = %@",notification.identifier);
     testCallBack();
 }
 
 - (void)userNotificationCenter:(NSUserNotificationCenter *)center didActivateNotification:(NSUserNotification *)notification
 {
     NSLog(@"用户点击了通知！");
+    NSLog(@"identifier = %@",notification.identifier);
+    NSLog(@"notification.response.string = %@",notification.response.string);
     testCallBack();
 }
 
 - (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)notification
 {
     //用户中心决定不显示该条通知(如果显示条数超过限制或重复通知等)，returen YES;强制显示
+    NSLog(@"identifier = %@",notification.identifier);
 //    return NO;
     testCallBack();
     return YES;
--- ztest/test_QSystemTrayIcon/znotification.mm
+++ ztest/test_QSystemTrayIcon/znotification.mm
@@ -5,16 +5,20 @@
 #include <AppKit/NSApplication.h>
 #include <Foundation/NSString.h>
 #include <QString>
-#include<QPixmap>
+#include <QPixmap>
 #include <QTemporaryFile>
 #include <QImageWriter>
+#include <QUuid>
 #include "zdelegate.h"
 
-
+//对应同个app,相同的identifier只显示一次，要实现相同的identifier，需要调用删除原来的通知
+//[[NSUserNotificationCenter defaultUserNotificationCenter] removeAllDeliveredNotifications];
 void znotification()
 {
+
     QString message("asdfasdfadf");
     QString title("asdfasdfadf");
+    QString identifier=QUuid::createUuid().toString();
 
     NSUserNotification *notification = [[NSUserNotification alloc] init];
     //当设置“横幅”的情况下，鼠标移动到图片时出现button
@@ -24,7 +28,8 @@ void znotification()
     //    notification.soundName = @"详细文字说明";
 
     // QString 转NSString
-    //    notification.title = [NSString stringWithUTF8String:title.toUtf8().data()];
+     notification.identifier=[NSString stringWithUTF8String:identifier.toUtf8().data()];
+//        notification.identifier = @"asdf";
 
     //通过资源文件，产生临时文件加载==================
     QPixmap notificationIconPixmap(":/image/4009_313.png");
@@ -62,19 +67,25 @@ void znotification()
     notification.actionButtonTitle = @"确定";
 //    notification.otherButtonTitle = @"取消";
 
+    //设置快速回复功能,在didActivateNotification通过notification.response.string获取到具体的字符串
+    notification.hasReplyButton = YES;
+
+
     //设置通知时间貌似没效果
     //设置通知提交的时间
     notification.deliveryDate = [NSDate dateWithTimeIntervalSinceNow:5];
-    //设置通知的循环(必须大于1分钟，估计是防止软件刷屏)
+//    设置通知的循环(必须大于1分钟，估计是防止软件刷屏)
     NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
     [dateComponents setSecond:70];
     notification.deliveryRepeatInterval = dateComponents;
     /**/
 
     [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
-    //    [[NSUserNotificationCenter defaultUserNotificationCenter] scheduleNotification:notification];
+//        [[NSUserNotificationCenter defaultUserNotificationCenter] scheduleNotification:notification];
 
     ZDelegate *delegate = [[ZDelegate alloc] init];
     [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:delegate];
+    //删除通知，以便相同的identifier能正常通知
+    [[NSUserNotificationCenter defaultUserNotificationCenter] removeAllDeliveredNotifications];
     return;
 }
