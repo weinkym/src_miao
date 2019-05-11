3382997caf9571d591c9293e44023e4cc86e3c48
miaozw <miaozw@yunxuetang.cn>
2019-05-11 14:34:38 +0800

    更新脚本

--- python/TEST_ALL/test/test_path.py
+++ python/TEST_ALL/test/test_path.py
@@ -1,6 +1,10 @@
-#!/usr/local/bin/python3
+##!/usr/local/bin/python3
 import os
+import platform
+import sys
 
+
+# encoding=utf8 
 def getPathList(path):
     fileList=[]
     for parent,dirnames,filenames in os.walk(path,  topdown=False):
@@ -8,14 +12,15 @@ def getPathList(path):
             if '/rundir/' in filename:
                 continue
             if filename[-3:] == ".py":
-                print("parent=",parent,"filename=",filename)
-                os.system("cd "+parent+";git add "+filename)
+                print("中parent=",parent,"filename=",filename)
+#                 # os.system("cd "+parent+";git add "+filename)
         
-
     return fileList
 
-
-# path="/Users/miaozw/Documents/TEMP/build-lj"
-path="/Users/miaozw/work/src_miao/python"
+# print("中parent=")
+# temp = sys.getdefaultencoding()  
+print(platform.python_version())
+path="/Users/miaozw/Documents/TEMP/build-lj"
+# path="/Users/miaozw/work/src_miao/python"
 
 getPathList(path)
