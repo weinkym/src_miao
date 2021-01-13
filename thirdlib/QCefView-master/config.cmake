a25364919aa3c44d35d37be567683947f86c7ac8
miaozw <weinkym@qq.com>
2021-01-13 09:47:57 +0800

    增加qcefview 库

---
+++ thirdlib/QCefView-master/config.cmake
@@ -0,0 +1,30 @@
+#
+# Build environment configuration file for QCefView
+#
+
+#
+# The Qt SDK path
+#
+set(QT_SDK_DIR
+  # Change this value to the Qt SDK path of your build environment
+  "$ENV{QTDIR}"
+)
+
+#
+# The root dir of the CEF SDK
+#
+set(CEF_SDK_DIR
+  # Change this value to the CEF binary distribution path of your build environment
+  "${CMAKE_CURRENT_SOURCE_DIR}/dep/cef_binary_75.1.14+gc81164e+chromium-75.0.3770.100_windows32"
+)
+
+#################################################################################
+#
+# For CI system
+#
+if (DEFINED ENV{APPVEYOR})
+  set(QT_SDK_DIR
+    # Change this value to the Qt SDK path of your build environment
+    "$ENV{QTDIR}"
+  )
+endif()
